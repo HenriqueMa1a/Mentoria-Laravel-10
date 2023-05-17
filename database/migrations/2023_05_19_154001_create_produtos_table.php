f0301023c43969f9dac25168043d3c6f2aef2aa4
HenriqueMa1a <henriquemaya01@icloud.com>
2023-05-17 12:48:25 -0300

    feature: populando banco com seeders

--- database/migrations/2023_05_19_154001_create_produtos_table.php
+++ database/migrations/2023_05_19_154001_create_produtos_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nome');
+            $table->decimal('valor', 10, 2);
+            $table->timestamps(); //create //update
         });
     }
 
