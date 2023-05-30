8c589d5d76c2476bb6784ae0193627611c8a2a89
HenriqueMa1a <henriquemaya01@icloud.com>
2023-05-30 12:24:27 -0300

    feature: Vendas - relacionamento da tabela e seeder

--- database/seeders/VendasSeeder.php
+++ database/seeders/VendasSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Venda;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class VendasSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+        Venda::create(
+            [
+                'numero_da_venda'=> 1,
+                'produto_id'=>8,
+                'cliente_id'=>6,
+            ]
+        );
+        Venda::create(
+            [
+                'numero_da_venda'=> 2,
+                'produto_id'=>9,
+                'cliente_id'=>7,
+            ]
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\VendaController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,7 +11,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-
+//Produtos
 // http://localhost:8989/produtos/maisAlgumacoisa
 Route::prefix('produtos')->group(function () {
     Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
@@ -22,7 +23,7 @@ Route::prefix('produtos')->group(function () {
     Route::put('/atualizarProduto/{id}', [ProdutosController::class, 'atualizarProduto'])->name('atualizar.produto');
     Route::delete('/delete', [ProdutosController::class, 'delete'])->name('produto.delete');
 });
-
+//Clientes
 Route::prefix('clientes')->group(function () {
     Route::get('/', [ClientesController::class, 'index'])->name('clientes.index');
     // Cadastrar Create
@@ -32,4 +33,11 @@ Route::prefix('clientes')->group(function () {
     Route::get('/atualizarCliente/{id}', [ClientesController::class, 'atualizarCliente'])->name('atualizar.cliente');
     Route::put('/atualizarCliente/{id}', [ClientesController::class, 'atualizarCliente'])->name('atualizar.cliente');
     Route::delete('/delete', [ClientesController::class, 'delete'])->name('cliente.delete');
+});
+//Vendas
+Route::prefix('vendas')->group(function () {
+    Route::get('/', [VendaController::class, 'index'])->name('vendas.index');
+    // Cadastrar Create
+    Route::get('/cadastrarVenda', [VendaController::class, 'cadastrarVenda'])->name('cadastrar.venda');
+    Route::post('/cadastrarVenda', [VendaController::class, 'cadastrarVenda'])->name('cadastrar.venda');
 });
